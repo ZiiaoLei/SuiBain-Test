31047b6f5d8c5895009714b650eb707d49e9690b
ZiiaoLei <mac@MacdeiMac-101.local>
2016-03-12 15:02:24 +0800

    zzzz

---
+++ IT03/IT03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IT03
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 ZI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IT03/IT03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IT03
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 ZI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
